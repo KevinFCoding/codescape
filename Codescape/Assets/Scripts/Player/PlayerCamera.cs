aece480108b7baf575d8cbcb648332ce1c64ec9f
AntoninP <antonin.pulby@edu.itescia.fr>
2020-02-19 11:17:57 +0100

    Add charcaters movements and animations

---
+++ Codescape/Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public enum RotationAxis
+    {
+        MouseX = 1,
+        MouseY = 2
+    }
+
+    [SerializeField] RotationAxis axes = RotationAxis.MouseX;
+    [SerializeField] float m_sensHorizontal = 10.0f;
+    [SerializeField] float m_sensVertical = 10.0f;
+
+    float minimumVert = -89.0f;
+    float maximumVert = 89.0f;
+
+    float _rotationX = 0;
+
+    private void Start()
+    {
+        LockCursor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (axes == RotationAxis.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * m_sensHorizontal, 0);
+        }
+        else if (axes == RotationAxis.MouseY)
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * m_sensVertical;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+
+            float rotationY = transform.localEulerAngles.y;
+
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+    }
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+}
