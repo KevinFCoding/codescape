586177e3df7d3c6509978f4ab8c95f347a5680d8
KF <kevin.fagot@edu.itescia.fr>
2020-02-20 09:38:51 +0100

    add base Interactable script

--- Codescape/Assets/Scripts/PlayerUI/Interactable.cs
+++ Codescape/Assets/Scripts/PlayerUI/Interactable.cs
@@ -4,6 +4,39 @@ public class Interactable : MonoBehaviour {
 
     public float radius = 3f; // range of interaction
 
+    bool IsInRange = false;
+    public bool IsDoorOpen = false;
+    Transform player;
+
+    public void Update() {
+            if (IsInRange) {
+            float Distance = Vector3.Distance(player.position, transform.position);
+
+            if (Distance <= radius) {
+                Debug.Log("INTERACT MOTHERFUCKER");
+                }
+            }
+          }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("lever1")) {
+            GameObject.FindGameObjectsWithTag("Door1");
+            }
+        }
+        
+    public void OpenDoor() {
+        IsDoorOpen = !IsDoorOpen;
+        }
+    //public void IsFocused(Transform playerTransform) {
+    //    IsInRange = true;
+    //    player = playerTransform;
+    //    }
+
+    //public void IsUnFocused() {
+    //    IsInRange = false;
+    //    player = null;
+    //    }
+
     private void OnDrawGizmosSelected() {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, radius);
